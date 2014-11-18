b2fa1c36ec23de45a7d57d13867673fc06471fe2
SquirrelKnight <ksquires11@gmail.com>
2014-11-18 02:37:52 -0800

    the wheel now locks at 720 degrees of turning wither direction and I ditched some magic numbers

--- ProjectFolder/Assets/TurningWheelScript.cs
+++ ProjectFolder/Assets/TurningWheelScript.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class TurningWheelScript : MonoBehaviour {
 
-public float turnSpeed = 1f;
+public float turnSpeed = 2f;
+public float highTurnSpeed = 5f;
+public float lowTurnSpeed = 0.5f;
+private float curRotation = 0;
+public float maxTurn = 720f;
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -12,14 +17,20 @@ public float turnSpeed = 1f;
 	// Update is called once per frame
 	void FixedUpdate () {
 	
+	if((-maxTurn<curRotation || Input.GetAxis("Horizontal") > 0) &&(curRotation < maxTurn || Input.GetAxis("Horizontal") < 0))
+	{
 	if(Input.GetKey(KeyCode.LeftControl)){
-		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*turnSpeed*0.5f));
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*lowTurnSpeed));
+		curRotation += Input.GetAxis("Horizontal")*lowTurnSpeed;
 	}
 	else if(Input.GetKey(KeyCode.LeftShift)){
-		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*turnSpeed*2f));
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*highTurnSpeed));
+		curRotation += Input.GetAxis("Horizontal")*highTurnSpeed;
 	}
 	else{
 		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*turnSpeed));
+		curRotation += Input.GetAxis("Horizontal")*turnSpeed;
+	}
 	}
 	}
 }
