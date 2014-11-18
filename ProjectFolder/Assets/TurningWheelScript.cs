ae43d5219cf49dc1211cfa37f7a0794117792e1f
SquirrelKnight <ksquires11@gmail.com>
2014-11-18 00:10:42 -0800

    spinning wheel spins faster with shift slower with control. still need to lock the wheel at a certain point

---
+++ ProjectFolder/Assets/TurningWheelScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurningWheelScript : MonoBehaviour {
+
+public float turnSpeed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	if(Input.GetKey(KeyCode.LeftControl)){
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*turnSpeed*0.5f));
+	}
+	else if(Input.GetKey(KeyCode.LeftShift)){
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*turnSpeed*2f));
+	}
+	else{
+		transform.Rotate(new Vector3(0,0,Input.GetAxis("Horizontal")*turnSpeed));
+	}
+	}
+}
