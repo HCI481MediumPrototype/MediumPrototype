cace2b8b3dfb0546f0baa51946c484844a4404c6
SquirrelKnight <ksquires11@gmail.com>
2014-11-18 20:10:19 -0800

    driving is a bit better could still use some work

--- ProjectFolder/Assets/DriveScript.cs
+++ ProjectFolder/Assets/DriveScript.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class DriveScript : MonoBehaviour {
 
-	public float speed = 0.5f;
+	public float acceleration = 0.01f;
+	public float brakeSpeed = 0.5f;
 	private float curSpeed = 0f;
-	public float turnDamper = 0.01f;
+	public float turnDamper = 0.02f;
+	public float maxSpeed = 1f;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,8 +16,21 @@ public class DriveScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.Translate(new Vector3(0,0,Input.GetAxis("Vertical")) * speed);
-		transform.Rotate(new Vector3(0,-TurningWheelScript.curRotation*turnDamper * Input.GetAxis("Vertical"),0));
+		if((Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow)) && curSpeed>0f)
+		{
+		curSpeed= curSpeed + (brakeSpeed * Input.GetAxis("Vertical"));
+		if(curSpeed<0)
+		{
+		curSpeed = 0;
+		}
+		}
+		else if(curSpeed<maxSpeed && (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow)))
+		{
+		curSpeed+=(acceleration * Input.GetAxis("Vertical"));
+		}
+		
+		transform.Translate(new Vector3(0,0,curSpeed));
+		transform.Rotate(new Vector3(0,-TurningWheelScript.curRotation*turnDamper * curSpeed,0));
 	}
 	
 	public float Velocity()
